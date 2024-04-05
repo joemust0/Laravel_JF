ec4509bdef9e5a463509c236f64c57514753c34c
joemust0 <joaofelipe.1607@gmail.com>
2024-04-04 22:09:59 -0300

    Alteração da paginal principal para o tema da atividade

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -12,36 +12,56 @@
 <body>
     <main class="bg-blue-100 rounded-xl">
     <h1>ETEC Zona Leste</h1>
-
+    <div class="login aligntext-right">
+    <a href="/">Login</a>
+    <a href="/contato">Cadastro</a>
+</div>
 <p>Bem vindo ao nosso site onde você pode ficar por dentro das nossas atualização</p>
-    
+
 <div class="main-header">
 <a href="/">Home</a>
 <a href="/contato">Contato</a>
-<a href="/sobrenos">Cursos</a>
-<a href="/sobrenos">Departamentos</a>
-<a href="/sobrenos">Oportunidades</a>
-<a href="/sobrenos">Vertibulinho</a>
 <a href="/sobrenos">Sobrenos</a>
 </div>
+<div class="estrutura bg-blue-300 text-blue-800 p-2 rounded m-5 p-5">
+    <img src="{{asset('img/tela_bloqueio_wallpaper_etec.png')}}"  alt="entrada alto">
+    <h3 class="text-black md:font-bold ">Estrutura</h3>
+    <p>Temos uma estrutura muito ampla para atender as demandas de estudo com acessibilidade para todos.</p>
+</div>
+<div class="espaco bg-blue-300 text-blue-800 p-2 rounded m-5 p-5 ">
+    <img src="{{asset('img/panoramica_etec.png')}}"  alt="panoramica_etec">
+    <h3 class="text-black md:font-bold">Espaço</h3>
+    <p class="mb-5">O espaço que forncemos é suficite para atender as demandas dos nossos cursos e capaciatar nossos alunos.</p>
+   <img src="{{asset('img/entrada.webp')}}"  alt="Espaço-entrada">
+    <p>Temos muita áreas em aberto e salas no mesmo nivel para facilitar a circulação dos alunos com necessidades especiais.</p>
+</div>
+<div class="espaco bg-blue-300 text-blue-800 p-2 rounded m-5 p-5 ">
+    <img src="{{asset('img/sala.webp')}}"  alt="Sala">
+    <h3 class="text-black md:font-bold ">Salas</h3>
+    <p>As salas estão prontas com otimos equipamentos para que os alunos possam se desenvolver.</p>
+</div>
+<div class="espaco bg-blue-300 text-blue-800 p-2 rounded m-5 p-5 ">
+    <img src="{{asset('img/auditório.webp')}}"  alt="Auditório">
+    <h3 class="text-black md:font-bold ">Auditório</h3>
+    <p>Contamos, com um exelente espaço para para apresentações, palestras e entre outros eventos para os alunos e comunidade.</p>
+</div>
+<div class="cursos m-10">
+<h3 class="text-black md:font-bold ">Cursos</h3>
+<p>Esses são os cursos que oferemos:</p>
+<ul>
+    <li>Administração</li>
+    <p>É o profissional que analisa e projeta sistemas. Constrói, docuemnta, e realiza produção de sistemas e páginas web.</p>
+    <li>Contabilidade</li>
+    <li>Desenvolvimento de Sistemas</li>
+    <li>Logística</li>
+    <li>Recursos Humanos</li>
+    <li>Serviços Jurídicos</li>
 
-<p>Baixo temos os uma Alguns dos modelos que que povoaram as ruas dos anos 90 em diante, com projetos diferênciados.</p>
-    
-<div class="grid grid-cols-4 gap-4 rounded">
-    <div class="bg-gray-500 text-white p-5 m-2 max-w-xl max-h-xs rounded-xl"><img src="{{asset('img/Alfa 164.jpg')}}"  alt="Alfa 164">Alfa 164</div>
-    <div class="bg-red-400 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/FIAT Coupe.jpg')}}"  alt="FIAT Coupé">FIAT Coupé</div>
-    <div class="bg-gray-500 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/FIAT Tempra.jpg')}}"  alt="FIAT Tempra">FIAT Tempra</div>
-    <div class="bg-red-400 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/FIAT Uno turbo i.e..jpg')}}" alt="FIAT Uno turbo i.e.">FIAT Uno turbo i.e.</div>
-    <div class="bg-gray-500 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/Ford Escort xr3.jpg')}}"  alt="Ford Escort xr3">Ford Escort xr3</div>
-    <div class="bg-red-400 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/GM Omega.jpg')}}"  alt="GM Omega">GM Omega</div>
-    <div class="bg-gray-500 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/GM Tigra.jpg')}}"  alt="GM Tigra">GM Tigra</div>
-    <div class="bg-red-400 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/Honda civic coupe.jpg')}}"  alt="Honda civic coupe">Honda civic coupe</div>
-    <div class="bg-gray-500 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/Honda-Civic-VTi.webp')}}"  alt="Honda-Civic-VTi">Honda-Civic-VTi</div>
-    <div class="bg-red-400 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/FIAT Coupe.jpg')}}"  alt="FIAT Coupé">FIAT Coupé</div>
-    <div class="bg-gray-500 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/vw-gol-1995.png')}}"  alt="vw-gol-1995">vw-gol-1995</div>
-    <div class="bg-red-400 text-white p-5 m-2 max-w-md max-h-xs rounded-xl"><img src="{{asset('img/WV Gol GTS.jpg')}}"  alt="WV Gol GTS">WV Gol GTS</div>
-
+</ul>
 </div>
 </main>
+<footer>
+
+</footer>
 </body>
 </html>
